14f469c299a2add5d5b4020b92728bc3eec9ded5
Ryomen Sukuna <155969848+RobertMentzel@users.noreply.github.com>
2024-01-09 13:36:53 +0100

    Fps kode

---
+++ re-vamp/Assets/TestEnemies/fps code.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fpscode : MonoBehaviour {
+
+    public float fps;
+    public Text fpsText;
+
+    private void Start()
+    {
+        InvokeRepeating("GetFPS", 1, 1);
+
+    }
+    public void GetFPS()
+
+    {
+        fps = (int)(1f / Time.unscaledDeltaTime) ;
+        fpsText.text = fps + "fps";
+    }
+
+}
