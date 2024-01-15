52d0fe6d1b72352e6648e45f9e5cd3529618dd00
Abdullahx-z <Abdullahx-z@users.noreply.github.com>
2024-01-15 12:37:51 +0100

    start af health script

---
+++ re-vamp/Assets/Abdellah/health.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
