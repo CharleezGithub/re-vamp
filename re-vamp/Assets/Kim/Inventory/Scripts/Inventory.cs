92e5ad82c9a922aa24b0a54283b0ab6cf4dc2ef6
kimx100 <kimrex@live.dk>
2024-01-17 22:30:49 +0100

    Inventory scripts + placeholder png

--- re-vamp/Assets/Kim/Inventory/Scripts/Inventory.cs
+++ re-vamp/Assets/Kim/Inventory/Scripts/Inventory.cs
@@ -1,18 +1,17 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<InventoryItem> items = new List<InventoryItem>();
+
+    public void AddItem(InventoryItem item)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RemoveItem(InventoryItem item)
     {
-        
+
     }
 }
---
+++ re-vamp/Assets/Kim/Inventory/Scripts/InventoryItem.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+[Serializable]
+public class InventoryItem
+{
+    public string itemName;
+    public int quantity;
+    public Image icon;
+    public string type;
+}
