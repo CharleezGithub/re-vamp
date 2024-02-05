83807072caf6ddc0ec8e4e8e0937a0ec1af32eba
kimrdd <kimrex@live.dk>
2024-02-05 12:42:29 +0100

    scene switcher name based

--- re-vamp/Assets/Scripts/Scene/SceneSwitcher.cs
+++ re-vamp/Assets/Scripts/Scene/SceneSwitcher.cs
@@ -3,13 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    // Angiv navnet på den scene, du vil skifte til
-    public string Scene2;
 
     // Metode kaldt, når knappen bliver trykket
     public void OnButtonPress()
     {
         // Skift scene til den angivne scene
-        SceneManager.LoadScene(Scene2);
+        SceneManager.LoadScene(this.name);
     }
 }
