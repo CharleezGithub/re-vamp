983cbe5121c783273b9018d5c51b97b4922fdf70
kimrdd <kimrex@live.dk>
2024-02-06 12:49:11 +0100

    error fix

--- re-vamp/Assets/PersonligeMapper/Samuel og Larsen/SwitchCharacter.cs
+++ re-vamp/Assets/PersonligeMapper/Samuel og Larsen/SwitchCharacter.cs
@@ -6,13 +6,15 @@ public class SwitchCharacter : MonoBehaviour
 {
     public int index = 0;
     public Texture2D gun;
-    public void Switch() {
+    public void Switch()
+    {
         index += 1;
-        gun = weapon_class[index];
+        //gun = weapon_class[index];
 
-   }
+    }
     public void Switchback()
     {
         index -= 1;
-        gun = weapon_class[index];
+        //gun = weapon_class[index];
     }
+}
