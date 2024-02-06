b53a468b40dc1d4f25080b55025ae66c801e5a3a
kimrdd <kimrex@live.dk>
2024-02-06 16:54:36 +0100

    trinket structure edit

--- re-vamp/Assets/Scripts/DataStructure/Trinkets.cs
+++ re-vamp/Assets/Scripts/DataStructure/Trinkets.cs
@@ -16,13 +16,15 @@ public class Trinket
 
 public class attribute
 {
-    public int max_health_incease { get; set; }
+    public int level { get; set; }
+    public int max_health_increase { get; set; }
     public int damage_increase { get; set; }
-    public int projectile_speed_increase { get; set; }
-    public int movement_speed_increase { get; set; }
     public int armor_increase { get; set; }
     public int item_shop_slot_increase { get; set; }
-    public int view_distance_increase { get; set; }
+    public float projectile_speed_increase { get; set; }
+    public float movement_speed_increase { get; set; }
+    public float view_distance_increase { get; set; }
+    public float level_multiplier { get; set; }
     public string description { get; set; }
     public string item { get; set; }
 }
