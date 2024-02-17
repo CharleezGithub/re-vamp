f47de2f9ac6ac263d8b7862e96ace390163f573c
kimrdd <kimrex@live.dk>
2024-02-17 18:15:48 +0100

    inventory and shop changes

---
+++ re-vamp/Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject[] weaponFields;
+    public GameObject[] trinketFields;
+
+    public Sprite[] weaponSprites;
+    public Sprite[] trinketSprites;
+
+    public Weapon weapon;
+    public Trinket trinket;
+
+}
