a8fa7612c84a2b9556dd8948a55088fe3b83d43d
kimx100 <kimrex@live.dk>
2024-02-18 13:39:44 +0100

    adaptive inventory done

--- re-vamp/Assets/Scripts/Player/Inventory.cs
+++ re-vamp/Assets/Scripts/Player/Inventory.cs
@@ -5,55 +5,64 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    public GameObject[] weaponFields;
-    public GameObject[] trinketFields;
+    public GameObject weaponRef; // Reference to the GameObject containing the Weapon script and parent to weapon fields
+    public GameObject trinketRef; // Reference to the GameObject containing the Trinket script and parent to trinket fields
 
-    public Weapon weapon;
-    public Trinket trinket;
+    public Vector2 fieldSize = new Vector2(100, 100); // Default field size
+    public Vector2 spriteSize = new Vector2(80, 80); // Default sprite size
+    public Vector2 weaponOffset = new Vector2(110, 0); // Offset for weapon fields
+    public Vector2 trinketOffset = new Vector2(0, -110); // Offset for trinket fields
 
-    public GameObject weaponRef;
-    public GameObject trinketRef;
-
-    public Vector2 fieldSize;
-    public Vector2 spriteSize;
+    public Sprite fieldSprite; // The background sprite for each field
 
     private void Start()
     {
-        InitializeFields(weaponFields, weaponRef);
-        InitializeFields(trinketFields, trinketRef);
+        InitializeFields();
     }
 
-    void InitializeFields(GameObject[] fields, GameObject referenceObject)
+    private void InitializeFields()
     {
-        foreach (GameObject field in fields)
+        Vector2 currentWeaponPosition = Vector2.zero; // Start position for weapon fields
+        Vector2 currentTrinketPosition = Vector2.zero; // Start position for trinket fields
+
+        Weapon weaponComponent = weaponRef.GetComponent<Weapon>();
+        if (weaponComponent != null)
         {
-            // Create a new GameObject for the field image
-            GameObject fieldImageGO = new GameObject("FieldImage");
-            fieldImageGO.transform.SetParent(field.transform); // Set parent
-
-            // Add and configure Image component
-            Image fieldImage = fieldImageGO.AddComponent<Image>();
-            fieldImage.color = Color.white; // Set the color to white for empty fields
-            fieldImage.rectTransform.sizeDelta = fieldSize;
-
-            // Optionally set the position of the fieldImage relative to its parent
-            fieldImage.rectTransform.localPosition = Vector3.zero;
-            fieldImage.rectTransform.localScale = Vector3.one; // Ensure scale is set to 1
-
-            // Create a new GameObject for the inner image (for weapon/trinket in use)
-            GameObject innerImageGO = new GameObject("InnerImage");
-            innerImageGO.transform.SetParent(fieldImageGO.transform); // Set parent to the field image
-
-            // Add and configure Image component for inner image
-            Image innerImage = innerImageGO.AddComponent<Image>();
-            innerImage.rectTransform.sizeDelta = spriteSize;
-
-            // Optionally set the position of the innerImage
-            innerImage.rectTransform.localPosition = Vector3.zero;
-            innerImage.rectTransform.localScale = Vector3.one; // Ensure scale is set to 1
-
-            // Initially, the inner image can also be set to a default color (transparent or any placeholder color)
-            innerImage.color = new Color(1, 1, 1, 0); // Fully transparent
+            foreach (var weaponData in weaponComponent.allWeaponData)
+            {
+                CreateInventoryItem(null, true, weaponRef.transform, ref currentWeaponPosition, weaponOffset);
+            }
         }
+
+        Trinket trinketComponent = trinketRef.GetComponent<Trinket>();
+        if (trinketComponent != null)
+        {
+            foreach (var trinketData in trinketComponent.allTrinketData)
+            {
+                CreateInventoryItem(null, false, trinketRef.transform, ref currentTrinketPosition, trinketOffset);
+            }
+        }
+    }
+
+    public void AddItem(Sprite itemSprite, bool isWeapon, int index)
+    {
+        // This function will now need to find the correct field based on index and update its sprite
+        // This part of the script will require adjustments to access and modify existing fields
+    }
+
+    void CreateInventoryItem(Sprite itemSprite, bool isWeapon, Transform parentObject, ref Vector2 currentPosition, Vector2 offset)
+    {
+        GameObject fieldGO = new GameObject(isWeapon ? "WeaponField" : "TrinketField", typeof(Image));
+        fieldGO.transform.SetParent(parentObject, false);
+        fieldGO.GetComponent<Image>().sprite = fieldSprite;
+
+        RectTransform fieldRect = fieldGO.GetComponent<RectTransform>();
+        fieldRect.sizeDelta = fieldSize;
+        fieldRect.anchoredPosition = currentPosition;
+
+        currentPosition += offset; // Update current position with offset for the next field
+
+        // The following part is modified to initially not create an item image inside the field
+        // Item images will be added or updated later through the AddItem method
     }
 }
