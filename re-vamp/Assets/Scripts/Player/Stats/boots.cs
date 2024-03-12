045bbd407045da4d65df824b8e3c3fbc197493e6
Charlee <charleegithub@gmail.com>
2024-03-12 12:45:55 +0100

    boots working

--- re-vamp/Assets/Scripts/Player/Stats/boots.cs
+++ re-vamp/Assets/Scripts/Player/Stats/boots.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class boots : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public PlayerMovement move_script;
+
+    public float speed_multiplier = 1.4f;
+
+    void OnEnable()
     {
-        
+        move_script.moveSpeed *= speed_multiplier;
     }
+    void OnDisable()
+    {
+        move_script.moveSpeed /= speed_multiplier;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
