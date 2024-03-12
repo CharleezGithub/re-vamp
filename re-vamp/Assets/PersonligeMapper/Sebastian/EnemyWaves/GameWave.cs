0d5e541d7d875f65d742a253b9a4455ec696e39f
kimrdd <kimrex@live.dk>
2024-03-12 13:20:51 +0100

    sync

--- re-vamp/Assets/PersonligeMapper/Sebastian/EnemyWaves/GameWave.cs
+++ re-vamp/Assets/PersonligeMapper/Sebastian/EnemyWaves/GameWave.cs
@@ -7,51 +7,44 @@ public class GameWave : MonoBehaviour
     public float WaveTime = 20f;
     public GameObject[] Enemies;
     public int Wave = 0;
+    public float waveDifficultyMult = 0.5f;
     public float radius;
-    // Start is called before the first frame update
-    void Start()
+    
+    private void FixedUpdate()
     {
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        timer += Time.deltaTime;
+        timer += 1f / 50;
 
         if (timer > WaveTime)
         {
-            SpawnWave(Wave);
+            SpawnWave(Wave * waveDifficultyMult, Wave);
             timer = 0f;
             Wave++;
         }
     }
 
-    private void SpawnWave(int Difficulty)
+    private void SpawnWave(float Difficulty, int wave)
     {
       while (Difficulty > 0)
         {
             int EnemyIndex = UnityEngine. Random.Range(0, Enemies.Length);
             var EnemyToSpawn = Enemies[EnemyIndex];
 
-            SpawnCircle(Difficulty, EnemyToSpawn);
+            SpawnInACircle(EnemyToSpawn);
             Difficulty -= 1;
         }
     }
-    void SpawnCircle(int numberOfObjects, GameObject objectToSpawn)
+
+    void SpawnInACircle(GameObject objectToSpawn)
     {
-        for (int i = 0; i < numberOfObjects; i++)
-        {
-            // Generate a random angle between 0 and 360 degrees
-            float angle = Random.Range(0f, 360f);
-            // Convert angle to radians and calculate x and y position
-            float x = Mathf.Cos(angle * Mathf.Deg2Rad) * radius;
-            float y = Mathf.Sin(angle * Mathf.Deg2Rad) * radius;
-            // Determine the position to spawn the object
-            Vector3 spawnPosition = transform.position + new Vector3(x, y, 0); // Adjust y to your needs, depending on the orientation of your game
-            // Spawn the object
-            Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
-        }
+        // Generate a random angle between 0 and 360 degrees
+        float angle = Random.Range(0f, 360f);
+        // Convert angle to radians and calculate x and y position
+        float x = Mathf.Cos(angle * Mathf.Deg2Rad) * radius;
+        float y = Mathf.Sin(angle * Mathf.Deg2Rad) * radius;
+        // Determine the position to spawn the object
+        Vector3 spawnPosition = transform.position + new Vector3(x, y, 0); // Adjust y to your needs, depending on the orientation of your game
+                                                                           // Spawn the object
+        Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
     }
 
 }
