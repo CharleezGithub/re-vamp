32f7a1cfe82277f76b5b7ca958a04a69cdfef0a9
BOT Alex <44818698+MagicBOTAlex@users.noreply.github.com>
2024-03-18 11:22:30 +0100

    added support for sounds on death

--- re-vamp/Assets/Scripts/Enemy/EnemyHealth.cs
+++ re-vamp/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -7,7 +7,10 @@ using UnityEngine.UI;
 public class EnemyHealth : MonoBehaviour
 {
     public int maxHealth = 100;
-    public int currentHealth;    
+    public int currentHealth;
+
+    public AudioClip PlayOnDeath;
+
     void Start()
     {
         currentHealth = maxHealth;
@@ -17,6 +20,8 @@ public class EnemyHealth : MonoBehaviour
     {
         if (currentHealth <= 0)
         {
+            if (PlayOnDeath != null)
+                AudioManager.PlaySound(PlayOnDeath, transform.position);
             Destroy(gameObject);
         }        
     }
