ecd1def31d61bd0c463b35d6f4dc91f490b17c5a
Charlee <charleegithub@gmail.com>
2024-04-02 12:21:37 +0200

    spelltome

---
+++ re-vamp/Assets/Scripts/Player/Attacks/SpellTome.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpellTome : MonoBehaviour
+{
+    [SerializeField] private GameObject projectilePrefab;
+    public float projectileSpeed;
+    public float projectileLifetime;
+    public float cooldownTime;
+
+    Vector2 direction;
+    bool isAttacking = false;
+
+    Transform playerTransform;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+
+        StartCoroutine(GetTopParent());
+    }
+
+    private void Update()
+    {
+        if (!isAttacking)
+            StartCoroutine(SlashAttack());
+    }
+
+    private IEnumerator GetTopParent()
+    {
+        Transform currentParent = transform.parent;
+
+        // Keep going up the hierarchy until there's no parent left
+        while (currentParent.parent != null)
+        {
+            currentParent = currentParent.parent;
+        }
+
+        playerTransform = currentParent;
+        yield return null;
+    }
+
+    private IEnumerator SlashAttack()
+    {
+        isAttacking = true;
+        yield return new WaitForSeconds(cooldownTime);
+        FireProjectile();
+        isAttacking = false;
+    }
+    void FireProjectile()
+    {
+        // Instantiate the projectile
+        GameObject projectile = Instantiate(projectilePrefab, playerTransform.position, Quaternion.identity);
+
+        // Move the projectile in the direction without rigidbody
+        StartCoroutine(MoveProjectile(projectile));
+
+        // Destroy the projectile after its lifetime
+        Destroy(projectile, projectileLifetime);
+    }
+    IEnumerator MoveProjectile(GameObject projectile)
+    {
+        while (projectile != null)
+        {
+            float step = projectileSpeed * Time.deltaTime;
+            direction = new Vector2(Random.Range(-1, 2), Random.Range(-1, 2));
+            projectile.transform.Translate(direction * step);
+            yield return null;
+        }
+    }
+}
