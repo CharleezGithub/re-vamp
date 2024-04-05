66e9f01cd8d5d4a26f91068c69440f3b89a2489f
BOTAlex <zhentao2004@gmail.com>
2024-04-05 20:21:23 +0200

    fixed build error

--- re-vamp/Assets/Scripts/DataStructure/TrinketData.cs
+++ re-vamp/Assets/Scripts/DataStructure/TrinketData.cs
@@ -24,7 +24,11 @@ public class TrinketData : ZItemSO, IZShopItem
     {
         if (trinketPrefab == null)
         {
-            string path = AssetDatabase.GetAssetPath(this);
+            string path = "NOT IN EDITOR";
+
+#if UNITY_EDITOR
+            path = AssetDatabase.GetAssetPath(this);
+#endif
             Debug.LogError($"Prefab not assigned! At: {path}\n(Kim, I know you're not reading this, but please just read errors my guy)");
 
             return null;
--- re-vamp/Assets/Scripts/DataStructure/WeaponData.cs
+++ re-vamp/Assets/Scripts/DataStructure/WeaponData.cs
@@ -17,7 +17,12 @@ public class WeaponData : ZItemSO, IZShopItem
     {
         if (weaponPrefab == null)
         {
-            string path = AssetDatabase.GetAssetPath(this);
+            string path = "NOT IN EDITOR";
+
+#if UNITY_EDITOR
+            path = AssetDatabase.GetAssetPath(this);
+#endif
+
             Debug.LogError($"Prefab not assigned! At: {path}\n(Kim, I know you're not reading this, but please just read errors my guy)");
 
             return null;
